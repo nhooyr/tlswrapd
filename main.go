6e4390535db7ff8337142b5c36459f29e38b8d1c
Anmol Sethi <anmol@aubble.com>
2016-10-08 11:36:59 -0400

    use configPath instead of working directory

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"os"
 	"runtime"
 
@@ -9,11 +10,13 @@ import (
 )
 
 func main() {
-	f, err := os.Open("config.json")
+	configPath := flag.String("c", "", "path to configuration file")
+	flag.Parse()
+
+	f, err := os.Open(*configPath)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	var proxies map[string]*proxy
 	err = json.NewDecoder(f).Decode(&proxies)
 	if err != nil {
